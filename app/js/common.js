58ba087a014d5dac99ba67e473fa6f8403a71683
Ekaterina194 <wasilkaa1994@gmail.com>
2017-06-18 13:47:11 +0300

    Сверстаны новости, галлерея мероп, мод окна

--- app/js/common.js
+++ app/js/common.js
@@ -185,6 +185,8 @@ $(document).ready(function () {
 
 	});
 
+
+
 	$('#b-f__zoom-gallery').magnificPopup({
 
 		delegate: 'a',
@@ -342,6 +344,32 @@ $(document).ready(function () {
 	});
 
 	$('#ab-p-tab-6__zoom-gallery').magnificPopup({
+
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+
+	});
+
+	$('.ev-g__gallary').magnificPopup({
 
 		delegate: 'a',
 		type: 'image',
